68713397fe8076e15bd20a5700f513c8ae57f85f
Beth Skurrie <beth@bethesque.com>
2026-05-04 11:59:00 +1000

    refactor: OO the alarm code

--- ecal/alarms/alarm.py
+++ ecal/alarms/alarm.py
@@ -10,30 +10,110 @@ from ecal.env import OUTPUT_AUDIO_DIRECTORY, INITIAL_VOLUME
 
 logger = logging.getLogger(__name__)
 
-def play_alarm(announcement_files, before_alarm_hook=None):
-    joined_announcement_file = OUTPUT_AUDIO_DIRECTORY + "/announcement.wav"
-    join_mp3s_to_wav(announcement_files, joined_announcement_file)
+"""
+Takes a list of CalenderDays and finds any alarms due within the given time window.
+"""
+class AlarmFinder:
+    def __init__(self, calendar_days, base_time, window):
+        self.calendar_days = calendar_days
+        self.base_time = base_time
+        self.window = window
 
-    audio_file = OUTPUT_AUDIO_DIRECTORY + "/alarm_mixed.wav"
+    def find_alarm_events(self):
+        start, end = self._get_time_window(self.base_time, self.window)
+        logging.info(
+            "Time window: %s → %s (WINDOW=%d mins)",
+            start.isoformat(),
+            end.isoformat(),
+            self.window)
+        return self._find_alarm_events_in_range(start, end)
 
-    alarm_file = get_alarm_file()
+    def _get_time_window(self, base_time, window_minutes):
+        # Round down to nearest multiple of WINDOW
+        minute = (self.base_time.minute // window_minutes) * window_minutes
+        start_time = self.base_time.replace(minute=minute, second=0, microsecond=0)
+        end_time = start_time + timedelta(minutes=window_minutes)
+        return start_time, end_time
 
-    build_alarm_audio(
-        announcement_file=joined_announcement_file,
-        alarm_file=alarm_file,
-        output_file=audio_file,
-        duration=300
-    )
+    def _find_alarm_events_in_range(self, start_time, end_time):
+        matching_events = []
+
+        for day in self.calendar_days:
+            for event in day.timed_events:
+                if event.alarm_time_within_window(start_time, end_time):
+                    matching_events.append(event)
+
+        return matching_events
+
+    def _log_results(self, results):
+        for result in results:
+            logging.info(
+                "Matched event: %s | %s",
+                result.start_time,
+                result.summary
+            )
+        logging.info("Total matched events: %d", len(results))
+
+"""
+Builds the alarm audio by using TTS to read out the event descriptions, and
+mixing in background alarm music.
+"""
+class AlarmAudio:
+    def __init__(self, events, base_time):
+        self.events = events
+        self.base_time = base_time
+
+    def build_alarm_file(self):
+        joined_announcement_file = OUTPUT_AUDIO_DIRECTORY + "/announcement.wav"
+        join_mp3s_to_wav(self._announcement_files_for_events(), joined_announcement_file)
+
+        audio_file = OUTPUT_AUDIO_DIRECTORY + "/alarm_mixed.wav"
+
+        alarm_file = self._get_alarm_file()
+
+        build_alarm_audio(
+            announcement_file=joined_announcement_file,
+            alarm_file=alarm_file,
+            output_file=audio_file,
+            duration=300
+        )
+        return audio_file
+
+    def _announcement_files_for_events(self):
+        return self._deduplicate_list([text_to_voice_file(self._announcement_for_event(event)) for event in self.events])
+
+    def _announcement_for_event(self, event):
+        summary = event.summary if event.summary else "an event"
+        if event.alarm_offset() > 0:
+            return f"It will be time for {summary} in {event.alarm_offset()} minutes"
+        else:
+            return f"It's time for {summary}"
+
+    def _get_alarm_file(self):
+        alarm_files = self._get_alarm_files()
+        # New alarm every 14 days
+        return select_item_by_date(sorted(alarm_files), self.base_time.date(), 14)
+
+    def _deduplicate_list(self, items):
+        return list(dict.fromkeys(items))
+
+    def _get_alarm_files(self):
+        # Get all mp3 files in the ALARMS_DIRECTORY
+        alarm_files = glob.glob(f"{ALARMS_DIRECTORY}/*.mp3")
+        if not alarm_files:
+            raise FileNotFoundError(f"No alarm files found in {ALARMS_DIRECTORY}")
+        return alarm_files
 
 
-    # Play the mixed audio file
+def play_alarm(audio_file, before_alarm_hook=None):
+    if before_alarm_hook:
+        before_alarm_hook()
     with mpd_connection() as alarm_player:
-        if before_alarm_hook:
-            before_alarm_hook()
         logger.info(f"Playing alarm {audio_file}")
         alarm_player.set_volume(INITIAL_VOLUME)
         alarm_player.play_file(audio_file)
         fade_up([(alarm_player, 100)], 45, 10)
+    # after alarm hook if nobody stops it?
 
 def stop_alarm(after_alarm_hook=None):
     # Stop alarm
@@ -54,73 +134,10 @@ def stop_alarm(after_alarm_hook=None):
 
     after_alarm_hook() if after_alarm_hook else None
 
-def parse_iso(dt_str):
-    return datetime.fromisoformat(dt_str)
-
-def deduplicate_list(items):
-    return list(dict.fromkeys(items))
-
-def get_time_window(base_time, window_minutes):
-    # Round down to nearest multiple of WINDOW
-    minute = (base_time.minute // window_minutes) * window_minutes
-    start_time = base_time.replace(minute=minute, second=0, microsecond=0)
-    end_time = start_time + timedelta(minutes=window_minutes)
-    return start_time, end_time
-
-def find_alarm_events_in_range(calendar_days, start_time, end_time):
-    matching_events = []
-
-    for day in calendar_days:
-        for event in day.timed_events:
-            if event.alarm_time_within_window(start_time, end_time):
-                matching_events.append(event)
-
-    return matching_events
-
-def log_results(results):
-    for result in results:
-        logging.info(
-            "Matched event: %s | %s",
-            result.start_time,
-            result.summary
-        )
-    logging.info("Total matched events: %d", len(results))
-
 def check_for_alarms(base_time, window, calendar_data, before_alarm_hook=None):
-    start, end = get_time_window(base_time, window)
+    events = AlarmFinder(calendar_data, base_time, window).find_alarm_events()
 
-    logging.info(
-        "Time window: %s → %s (WINDOW=%d mins)",
-        start.isoformat(),
-        end.isoformat(),
-        window
-    )
-    results = find_alarm_events_in_range(calendar_data, start, end)
-    log_results(results)
+    if events:
+        alarm_audio_file = AlarmAudio(events, base_time).build_alarm_file()
+        play_alarm(alarm_audio_file, before_alarm_hook)
 
-    if results:
-        play_alarm(announcement_files_for_events(results), before_alarm_hook)
-
-def announcement_files_for_events(events):
-    return deduplicate_list([text_to_voice_file(announcement_for_event(event)) for event in events])
-
-def announcement_for_event(event):
-    summary = event.summary if event.summary else "an event"
-    if event.alarm_offset() > 0:
-        return f"It will be time for {summary} in {event.alarm_offset()} minutes"
-    else:
-        return f"It's time for {summary}"
-
-def get_alarm_file(date=None):
-    alarm_files = get_alarm_files()
-    if date is None:
-        date = datetime.now().date()
-    # New alarm every 14 days
-    return select_item_by_date(sorted(alarm_files), date, 14)
-
-def get_alarm_files():
-    # Get all mp3 files in the ALARMS_DIRECTORY
-    alarm_files = glob.glob(f"{ALARMS_DIRECTORY}/*.mp3")
-    if not alarm_files:
-        raise FileNotFoundError(f"No alarm files found in {ALARMS_DIRECTORY}")
-    return alarm_files
