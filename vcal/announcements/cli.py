9a1048aeb8eb1a83999526cdecb6cd137f86efa3
Beth Skurrie <beth@bethesque.com>
2026-05-07 09:38:43 +1000

    chore: refactor

---
+++ vcal/announcements/cli.py
@@ -0,0 +1,43 @@
+from vcal.announcements.announce import play_morning_announcements_audio_file, MORNING_ANNOUNCEMENTS_AUDIO_FILE
+from datetime import datetime
+import argparse
+from vcal.env import DATA_DIRECTORY, LOG_LEVEL
+import os
+from vcal.announcements.announce import play_morning_announcements as do_play_morning_announcements, play_morning_announcements_audio_file, SPEECH_FILE
+from vcal.log_config import setup_logging_for_announcements
+from vcal.scene import Scene
+
+setup_logging_for_announcements(str(LOG_LEVEL))
+
+def play_morning_announcements():
+    parser = argparse.ArgumentParser(description="Check for alarms in calendar events")
+    parser.add_argument(
+        "--cached",
+        action="store_true",
+        help="Only announce cached events"
+    )
+
+    parser.add_argument(
+        "--base_time",
+        type=lambda s: datetime.fromisoformat(s),
+        default=None,
+        help="Base time for checking alarms (ISO format, defaults to current time)"
+    )
+
+    parser.add_argument(
+        "--calendar_file",
+        default=os.path.join(DATA_DIRECTORY, "calendar.json"),
+        help=f"Path to the calendar JSON file (default: {os.path.join(DATA_DIRECTORY, 'calendar.json')})"
+    )
+
+    args = parser.parse_args()
+    base_time = args.base_time or datetime.now().astimezone()
+
+    if args.cached:
+        play_morning_announcements_cached()
+    else:
+        do_play_morning_announcements(args.calendar_file, base_time, Scene.prepare, Scene.restore)
+
+
+def play_morning_announcements_cached():
+    play_morning_announcements_audio_file(MORNING_ANNOUNCEMENTS_AUDIO_FILE, Scene.prepare, Scene.restore)
