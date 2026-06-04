ff4f2609594bc311962458b9ddc517558d7d9569
Beth Skurrie <beth@bethesque.com>
2026-06-04 19:40:55 +1000

    chore: something

--- server/tests/manual/prod_one_off_announcement_test.sh
+++ server/tests/manual/prod_one_off_announcement_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -X POST https://nas.dixon.net.au:8443/announce -d '{"message": "This is a one-off announcement test"}' -H "Content-Type: application/json"
+curl -X POST https://nas.dixon.net.au:8443/announce -d '{"message": "This is a one-off announcement test"}' -H "Content-Type: application/json" -k
