47b92e478fafd5d23b8bfee2a82ddf00c594dc5b
Beth Skurrie <beth@bethesque.com>
2026-06-12 14:58:23 +1000

    chore: add timing to deploy scripts

--- script/deploy/audio_host.sh
+++ script/deploy/audio_host.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-ansible-playbook -i ansible/inventory/hosts.yml --limit audio_host ansible/audio_host.yml -e @ansible/secrets.yml "$@"
+ANSIBLE_FORCE_COLOR=true  ansible-playbook -i ansible/inventory/hosts.yml --limit audio_host \
+    ansible/audio_host.yml -e @ansible/secrets.yml "$@" \
+    2>&1 | ts '[%Y-%m-%d %H:%M:%S]'
+
--- script/deploy/bluetooth_button_listener.sh
+++ script/deploy/bluetooth_button_listener.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-ansible-playbook -i ansible/inventory/hosts.yml --limit bluetooth_button_listeners ansible/bluetooth_button_listeners.yml -e @ansible/secrets.yml "$@"
+ANSIBLE_FORCE_COLOR=true  ansible-playbook -i ansible/inventory/hosts.yml \
+    --limit bluetooth_button_listeners ansible/bluetooth_button_listeners.yml \
+    -e @ansible/secrets.yml "$@" \
+    2>&1 | ts '[%Y-%m-%d %H:%M:%S]'
