16efc8255ce659fc208200f67292285a7349e6ce
Beth Skurrie <beth@bethesque.com>
2026-06-27 16:51:55 +1000

    chore: smooth vol changes

--- server/vcal/music_assistant.py
+++ server/vcal/music_assistant.py
@@ -266,7 +266,7 @@ class MusicAssistant:
 
     def restore_volume(self):
         playing_players = [player for player in self.players if player.get_original_state().playing()]
-        fade_up([(player, player.get_original_state().get_volume()) for player in playing_players], duration=3, steps=4)
+        fade_up([(player, player.get_original_state().get_volume()) for player in playing_players], duration=3, steps=12)
 
     def restore_original_state(self):
         playing_players = [player for player in self.players if player.get_original_state().playing()]
@@ -277,7 +277,7 @@ class MusicAssistant:
             # give the buffers time to get their glitches out
             time.sleep(1)
 
-            fade_up([(player, player.get_original_state().get_volume()) for player in playing_players], duration=5, steps=10)
+            fade_up([(player, player.get_original_state().get_volume()) for player in playing_players], duration=3, steps=12)
 
     def playing(self) -> bool:
         return any(player.get_original_state().playing() for player in self.players)
