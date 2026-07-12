52a5b0163543d06290ebc302ef52b6efbfc1634d
Beth Skurrie <beth@bethesque.com>
2026-07-12 14:48:54 +1000

    chore: try softer bell

--- server/vcal/announcements/announce.py
+++ server/vcal/announcements/announce.py
@@ -23,7 +23,7 @@ SILENCE_5_SEC = "audio/silence_5s.mp3"
 SILENCE_1_SEC = "audio/silence_1s.mp3"
 SILENCE_HALF_SEC = "audio/silence_500ms.mp3"
 
-PRE_ANNOUNCEMENT_BELL = AUDIO_DIRECTORY + "/preannounce_2.mp3"
+PRE_ANNOUNCEMENT_BELL = AUDIO_DIRECTORY + "/preannounce_3.mp3"
 from enum import Enum
 
 logger = logging.getLogger(__name__)
