8a4ef2f3a10f47ab3c13fee6c70efe9428dc05f1
Beth Skurrie <beth@bethesque.com>
2026-07-21 16:54:47 +1000

    chore: update manual test scripts

--- server/tests/manual/local_text_announcement_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Dev only, ensure the index.py server and mpd are running..."
-
-curl -X POST http://127.0.0.1:8081/announce -d '{"message": "This is a one-off announcement test"}' -H "Content-Type: application/json"
-sleep 5
-curl -X POST "http://127.0.0.1:8081/announce?message=This+is+a+one-off+announcement+test&players=kaypi&players=travcal"
---
+++ server/tests/manual/local_tts_announcement_test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Dev only, ensure the index.py server and mpd are running..."
+
+curl -X POST http://127.0.0.1:8081/announce -d '{"message": "This is a one-off announcement test"}' -H "Content-Type: application/json"
+#curl -X POST "http://127.0.0.1:8081/announce?message=This+is+a+one-off+announcement+test&players=kaypi&players=travcal"
+#curl -X POST "http://127.0.0.1:8081/announce?message=This+is+a+one-off+announcement+test&players=kaypi&players=travcal"
+#curl -X POST "http://127.0.0.1:8081/announce?message=This+is+a+one-off+announcement+test&players=kaypi&players=travcal"
+
