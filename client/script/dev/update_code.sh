d5f31c89c0711b1da67bd7bdd4165302de442d97
Beth Skurrie <beth@bethesque.com>
2026-08-10 13:17:25 +1000

    chore: update update_code.sh

--- client/script/dev/update_code.sh
+++ client/script/dev/update_code.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 script_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
-command="cd calendar-alarms && git sparse-checkout add client && git pull && systemctl --user restart audio-client-http"
-command="systemctl --user restart audio-client-http"
-"$script_dir"/../../../script/dev/run_command_on_clients.sh "${command}"
+command_1="cd calendar-alarms && git sparse-checkout add client && git pull && systemctl --user restart audio-client-http"
+command_2="systemctl --user restart audio-client-http"
+"$script_dir"/../../../script/dev/run_command_on_clients.sh "${command_1};${command_2}"
