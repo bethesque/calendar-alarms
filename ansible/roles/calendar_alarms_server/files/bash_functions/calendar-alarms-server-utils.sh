802b951c476e585a9732086229a8cf83cbefb48a
Beth Skurrie <beth@bethesque.com>
2026-09-08 08:11:51 +1000

    chore: add functions for showing systemctl timers

--- ansible/roles/calendar_alarms_server/files/bash_functions/calendar-alarms-server-utils.sh
+++ ansible/roles/calendar_alarms_server/files/bash_functions/calendar-alarms-server-utils.sh
@@ -77,6 +77,15 @@ function calendar-alarms-morning-announcements-logs {
     journalctl SYSLOG_IDENTIFIER=calendar-alarms-morning-announcements "$@"
 }
 
+function calendar-alarms-morning-announcements-timer {
+    systemctl --user list-timers calendar-alarms-morning-announcements.timer
+}
+
 function calendar-alarms-school-announcements-logs {
     journalctl SYSLOG_IDENTIFIER=calendar-alarms-school-announcements "$@"
 }
+
+function calendar-alarms-school-announcements-timer {
+    systemctl --user list-timers calendar-alarms-school-announcements.timer
+}
+
