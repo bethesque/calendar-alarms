c4d4e213d93c0b3b5b5eef2e8fb821e6376d395d
Beth Skurrie <beth@bethesque.com>
2026-09-12 07:34:52 +1000

    feat: add support for ferny creek

---
+++ script/deploy/dwain_audio_host.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ANSIBLE_FORCE_COLOR=true  ansible-playbook -i ansible/inventory/dwain_hosts.yml --limit audio_host \
+    ansible/audio_host.yml -e @ansible/secrets.yml "$@" \
+    2>&1 | ts '[%Y-%m-%d %H:%M:%S]'
