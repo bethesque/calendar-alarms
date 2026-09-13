a67db8791dccf72f68dbf5e3043c1c9aab69db3d
Beth Skurrie <beth@bethesque.com>
2026-09-13 13:38:58 +1000

    chore: update dwainpi as well

--- server/script/deploy/audio_host_codebase.sh
+++ server/script/deploy/audio_host_codebase.sh
@@ -4,3 +4,7 @@ git push
 ssh travnas 'cd calendar-alarms && git pull'
 ssh travnas 'systemctl --user restart calendar-alarms-http.service'
 ssh travnas 'systemctl --user restart calendar-alarms.service'
+
+ssh pi@dwainpi.local 'cd calendar-alarms && git pull'
+ssh pi@dwainpi.local 'systemctl --user restart calendar-alarms-http.service'
+ssh pi@dwainpi.local 'systemctl --user restart calendar-alarms.service'
