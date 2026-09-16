f9c30e3c236fc7d078b7fa56dccbb0c2842b91ed
Beth Skurrie <beth@bethesque.com>
2026-09-16 13:39:56 +1000

    chore: comments

--- server/script/deploy/audio_host_codebase.sh
+++ server/script/deploy/audio_host_codebase.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 git push
+echo "Updating travnas"
 ssh travnas 'cd calendar-alarms && git pull'
+echo "Restarting calendar-alarms-http.service"
 ssh travnas 'systemctl --user restart calendar-alarms-http.service'
+echo "Restarting calendar-alarms.service"
 ssh travnas 'systemctl --user restart calendar-alarms.service'
 
+echo "Updating dwainpi"
 ssh pi@dwainpi.local 'cd calendar-alarms && git pull'
+echo "Restarting calendar-alarms-http.service"
 ssh pi@dwainpi.local 'systemctl --user restart calendar-alarms-http.service'
+echo "Restarting calendar-alarms.service"
 ssh pi@dwainpi.local 'systemctl --user restart calendar-alarms.service'
